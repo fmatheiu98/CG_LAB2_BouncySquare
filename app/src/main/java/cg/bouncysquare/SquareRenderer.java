fa2903580c2eaa329a90608392b2e1f2b58ca326
fmatheiu98 <matheiuflavius@gmail.com>
2020-02-25 11:29:01 +0200

    First Commit

---
+++ app/src/main/java/cg/bouncysquare/SquareRenderer.java
@@ -0,0 +1,47 @@
+package cg.bouncysquare;
+import javax.microedition.khronos.opengles.GL10;
+import javax.microedition.khronos.egl.EGLConfig;
+import android.opengl.GLSurfaceView;
+import java.lang.Math;
+
+public class SquareRenderer implements GLSurfaceView.Renderer {
+
+    private Square mSquare;
+    private float mTransY=0;
+    public SquareRenderer()
+    {
+
+        mSquare = new Square();
+    }
+    public void onDrawFrame(GL10 gl)
+    {
+
+        gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
+        gl.glMatrixMode(GL10.GL_MODELVIEW);
+        gl.glLoadIdentity();
+        gl.glTranslatef(0.0f,(float)Math.sin(mTransY), -3.0f);
+        mTransY += 0.3f;
+        gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
+        gl.glEnableClientState(GL10.GL_COLOR_ARRAY);
+        mSquare.draw(gl);
+    }
+
+    public void onSurfaceChanged(GL10 gl, int width, int height)
+    {
+        gl.glViewport(0, 0, width, height);
+        gl.glMatrixMode(GL10.GL_PROJECTION);
+        gl.glLoadIdentity();
+        float ratio = (float) width / height;
+        gl.glFrustumf(-ratio, ratio, -1, 1, 1, 10);
+    }
+
+    public void onSurfaceCreated(GL10 gl, EGLConfig config)
+    {
+        //gl.glDisable(GL10.GL_DITHER);
+        gl.glHint(GL10.GL_PERSPECTIVE_CORRECTION_HINT, GL10.GL_FASTEST);
+        gl.glClearColor(0,0,50,100);
+        gl.glEnable(GL10.GL_CULL_FACE);
+        gl.glShadeModel(GL10.GL_SMOOTH);
+        gl.glEnable(GL10.GL_DEPTH_TEST);
+    }
+}
